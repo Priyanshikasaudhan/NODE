611778a6f9c39688fdaaaf0b35ec441c37d04028
Priyanshi <priyanshikasaudhan8429@gmail.com>
2025-02-16 00:52:43 +0530

    learn

--- L-5/Calculator/handler.js
+++ L-5/Calculator/handler.js
@@ -16,19 +16,20 @@ const request = (req, res) => {
         res.write('<html>');
         res.write('<head><title>Calculator</title></heaad>');
         res.write('<body><h1>Calculator</h1>'); 
-        res.write('<form action="/calculate" method="POST">');
+        res.write('<form action="/calculate-result" method="POST">');
         res.write('<input type="text" name="num1" palceholder="Enter first number"> <br>');
         res.write('<input type="text" name="num2" palceholder="Enter second number"> <br>');
-        res.write('<button type="submit">Calculate</button>');
+        res.write('<button type="submit" value="Sum">Calculate</button>');
         res.write('</form>');
         res.write('</body>');
-        res.write('</html>');
-        return res.end();}
+        res.write('</html>'); 
+        return res.end();}else if(req.url.toLowerCase() === "/calculate-result" && req.method === "POST"){
+            return sumRequestHandler(req, res);     
+        }
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>Calculator</title></heaad>');
-    res.write('<body><h1>404 pages does not Exist</h1>'); 
-    res.write('<a href="/calculator"><h1>Calculator</h1></a>');
+    res.write('<body><h1>Your Sum is ${result}</h1>'); 
     res.write('</body>');
     res.write('</html>');
     return res.end();
